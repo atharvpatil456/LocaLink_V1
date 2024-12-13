a80a785f2ed6f1bb4613e9958ae6477fd5948ccc
Atharv Patil <115338523+atharvpatil456@users.noreply.github.com>
2024-12-13 16:27:12 +0530

    Update LocaLinkApplication.java

--- src/main/java/com/athar/LocaLink/LocaLinkApplication.java
+++ src/main/java/com/athar/LocaLink/LocaLinkApplication.java
@@ -15,17 +15,25 @@ import com.google.cloud.firestore.v1.FirestoreClient;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Base64;
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class LocaLinkApplication {
 
 	public static void main(String[] args) throws IOException  {
 		ClassLoader classLoader = LocaLinkApplication.class.getClassLoader();
 		 ApiKey key = new ApiKey();
+		String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+            byte[] decodedBytes = Base64.getDecoder().decode(firebaseConfig);
+            InputStream serviceAccount = new ByteArrayInputStream(decodedBytes);
 		
 		//File file  = new 
 				//File(Objects.requireNonNull(classLoader.getResource(key.json)).getFile());
 		
-		FileInputStream serviceaccount = new FileInputStream("src/main/resources/"+key.json);
+		//FileInputStream serviceaccount = new FileInputStream("src/main/resources/"+key.json);
 		
 		
 		FirebaseOptions options = new FirebaseOptions.Builder()
